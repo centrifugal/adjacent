fcf64084484690a9101f2e784c67e25cd3a5940c
FZambia <frvzmb@gmail.com>
2015-02-24 17:26:37 +0300

    v0.1.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def long_description():
 
 setup(
     name='adjacent',
-    version='0.1.0dev',
+    version='0.1.0',
     description="Centrifuge integration with Django framework",
     long_description=long_description(),
     url='https://github.com/centrifugal/adjacent',
